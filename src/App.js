2bd587e95f280d0923bf4e12261d11adcb13bf0f
sametkoyuncu <sametkoyuncu@live.com>
2022-07-19 13:02:37 +0300

    Add all files from lamadev repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useState } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
+import { darkTheme, lightTheme } from './utils/Theme'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Video from './pages/Video'
+import SignIn from './pages/SignIn'
+
+const Container = styled.div`
+  display: flex;
+`
+// https://youtu.be/yIaXoop8gl4?t=875
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`
+
 function App() {
-  return <div>Hello world!</div>
+  const [darkMode, setDarkMode] = useState(true)
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App
